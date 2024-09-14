70d62878ae13593d4ab48c504ed09141d1a0d751
evaristocuesta <evaristocg@gmail.com>
2024-09-14 23:29:44 +0200

    Add tests

--- dist/index.js
+++ dist/index.js
@@ -31095,6 +31095,7 @@ var __webpack_exports__ = {};
 var exports = __webpack_exports__;
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.run = run;
 const core_1 = __nccwpck_require__(2186);
 const github_1 = __nccwpck_require__(5438);
 async function run() {
@@ -31118,7 +31119,9 @@ async function run() {
         (0, core_1.setFailed)((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : 'Unknown error');
     }
 }
-run();
+if (!process.env.JEST_WORKER_ID) {
+    run();
+}
 
 })();
 
